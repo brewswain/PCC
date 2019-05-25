46f3eac63bdd4714c05ee82266aa3669ad74cc38
brewswain <brandonlee2543@hotmail.com>
2019-05-24 20:19:59 -0400

    remove duplicates

--- Part I - Basics/Chapter 5 If Statements/Notes/2_conditional_tests.py
+++
@@ -1,6 +0,0 @@
-"""
-At the heart of every if statement is an expression that can be either true or false.
-This is called a conditional test.
-If a conditional test evaluates to "True", Python executes the cde following the if statement.
-If False, python ignores the code following the if statement.
-"""

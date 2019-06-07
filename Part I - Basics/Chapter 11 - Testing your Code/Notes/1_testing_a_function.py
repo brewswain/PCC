05400a8b7b743469e92e8ff6abb1a1569c14e6d0
brewswain <brandonlee2543@hotmail.com>
2019-06-07 17:10:30 -0400

    Begin Chapter 11.

---
+++ Part I - Basics/Chapter 11 - Testing your Code/Notes/1_testing_a_function.py
@@ -0,0 +1,57 @@
+"""
+To learn about testing, we need code4 to test. Here's a simple function
+that takes in a  first and last name, and returns a neatly formatted
+full name:
+"""
+# name_function.py
+
+
+def get_formatted_name(first, last):
+    """Generate a neatly formatted full name."""
+    full_name = first + " " + last
+    return full_name.title()
+
+
+"""
+The function get_formatted_name() combines the first and last name with
+a space in between to complete a full name, and then capitalizes and
+returns the full name. To check that get_formatted_name() works, let's
+make a program that uses this function. the program names.py lets users
+enter a first and last name, and see a neatly formatted full name:
+"""
+# names.py
+
+print("enter 'q' at any time to quit.")
+while True:
+    first = input("\nPlease give me a first name: ")
+    if first == 'q':
+        break
+    last = input("Please give me a last name: ")
+    if last == 'q':
+        break
+
+    formatted_name = get_formatted_name(first, last)
+    print("\tNeatly formatted name: " + formatted_name + '.')
+
+"""
+The user can enter a series of first and last names, and see the formatted
+full names that are generated:
+
+=============================================
+|   # Please give me a first name: brandon  |
+|   # Please give me a last name: lee       |
+|   # Neatly formatted name: Brandon Lee.   |
+=============================================
+
+We can see that the names generated here are correct. But let's say we want
+to modify get_formatted_name() so it can also handle middle names. As we do
+so, we want to make sure we don't break the way the function handles names 
+that only have a first and last name. We could test our code by running
+names.py and entering a name like Janis Joplin every time we modify
+get_formatted_name(), but that would become tedious. 
+
+Fortunately, Python provides an efficient way to automate the testing of a 
+function's output. If we automate the testing of get_formatted_name(), we 
+can always be confident that the function will work when given the kinds
+of names we've written tests for.
+"""
---
+++ Part I - Basics/Chapter 11 - Testing your Code/Notes/3_a_passing_test.py
@@ -0,0 +1,76 @@
+"""
+The syntax for setting up a test case takes some getting used to, but once 
+you've set up the test case it's straightforward to add more unit tests for
+your functions. To write a test case for a function, import the unittest
+module and the function you want to test. Then, create a class that inherits
+from unittest.TestCase, and write a series of methods to test different 
+aspects of your function's behaviour.
+
+Here's a test case with one method that verifies that the function 
+get_formatted_name() - Located in 1_testing_a_function.py, works correctly 
+when given a first and last name:
+"""
+
+import unittest
+from name_function import get_formatted_name
+
+
+class NameTestCase(unittest.TestCase):
+    """Tests for 'name_function.py'."""
+
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+
+
+unittest.main()
+
+
+"""
+First we import unittest and the function we want to test, get_formatted_name().
+At line 18, we create a class called NamesTestCase, which will contain a series of
+unit tests for get_formatted_name() . You can name the class anything you want, 
+but it's best to call it something related to the function you're about to test and
+use the word Test in the class name. This class MUST inherit from the class 
+'unittest.TestCase' so Python knows how to run the tests you write.
+
+NamesTestCase contains a single method that tests one aspect of get_formatted_name().
+We call this method test_first_last_name() because we're verifying that names with
+only a first and last name are formatted correctly.  Any method that starts with
+'test_' will be run automatically when we run this program. Within this test method,
+we call the function we want to test and store a return value that we're interested 
+in testing. In this example we call get_formatted_name() with the arguments 'janis'
+and 'joplin', and store the result in formatted_name(line 23).
+
+At line 24, we use one of unittest's most useful features: An assert method. Assert
+methods verify that a result you received matches the result you expected to receive.
+In this case, because we know get_formatted_name() is supposed to return a 
+capitalized, properly spaced full name, we expect the value in formatted_name to be
+'Janis Joplin'. To check if this is true, we use unittest's assertEqual() method and 
+pass it formatted_name and 'Janis Joplin'. 
+
+The line "self.assertEqual(formatted_name, 'Janis Joplin')"says, "Compare the value 
+in formatted_name to the string 'Janis Joplin'. If they are equal as expected, fine. 
+But if they don't match, let me know."
+
+The line unittest.main() tells Python to run the tests in this file. When we run this
+program, we get the following output:
+
+=================================================
+|   .                                           |
+|   ------------------------------------------  |
+|   Ran 1 test in 0.000s                        |
+|                                               |
+|   OK                                          |
+=================================================
+
+The dot on the firs line of output tells us that a single test passed. The next line
+tells us that Python ran one test, and it took less than 0.001 seconds to run. The
+final OK tells us that all unit tests in the test case passed.
+
+This output indicates that the function get_formatted_name() will always work for names
+that have a first and last name unless we modify the function. When we modify 
+get_formatted_name(), we can run this test again. If the test case passes, we know the 
+function will still work for names like Janis joplin.
+"""

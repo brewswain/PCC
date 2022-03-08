bbbc9d14e76d4a504b8c4c5c00b0140e9983cc27
Brandon Lee <brandonlee2543@hotmail.com>
2022-03-08 18:20:05 -0400

    fixed annoying typos knowing well sure that there's more

--- Part I - Basics/Chapter 09 - Files and Exceptions/Notes/01_reading_from_a_file.py
+++ Part I - Basics/Chapter 09 - Files and Exceptions/Notes/01_reading_from_a_file.py
@@ -3,11 +3,11 @@ An incredible amount of data is available in text files. text files can contain 
 traffic data, socioeconomic data, literary works, and more. Reading from a file is particularly
 useful in data analysis applications, but it's also applicable to any situation in which you
 want to analyze or modify information stored in a file. For example, you can write a program
-that rteads in the contents of a text file and rewrites the file with formatting that allows
+that reads in the contents of a text file and rewrites the file with formatting that allows
 a browser to display it.
 
-When you want ot work with the information in a text file, the first step is to read the file
-into memory. You can read the entire contents of a file, yor you can work through the file
+When you want to work with the information in a text file, the first step is to read the file
+into memory. You can read the entire contents of a file, or you can work through the file
 one line at a time.
 """
 
